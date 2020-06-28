e6113fbc84c40ef88a2cf443edecd5fa610041c1
Gregor Wolf <gregor.wolf@gmail.com>
2020-06-28 14:47:52 +0200

    Working version

--- srv/server.js
+++ srv/server.js
@@ -3,10 +3,13 @@
 const cds = require("@sap/cds")
 const proxy = require("@sap/cds-odata-v2-adapter-proxy")
 
-cds.on('bootstrap', (app)=>{
-  app.use(proxy({
-    path: "sap/opu/odata/sap"
-  }))
-})
+const port = process.env.PORT || 4004;
+
+cds.on("bootstrap", app => app.use(
+  proxy({
+    path: "sap/opu/odata/sap",
+    port: port
+  })
+));
 
 module.exports = cds.server
