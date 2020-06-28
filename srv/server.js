31bfcd8c00042f158a2c96ef3595aa0ea4a562ec
Gregor Wolf <gregor.wolf@gmail.com>
2020-06-28 08:59:35 +0200

    Update packages, test

--- srv/server.js
+++ srv/server.js
@@ -4,7 +4,9 @@ const cds = require("@sap/cds")
 const proxy = require("@sap/cds-odata-v2-adapter-proxy")
 
 cds.on('bootstrap', (app)=>{
-  app.use(proxy())
+  app.use(proxy({
+    path: "sap/opu/odata/sap"
+  }))
 })
 
 module.exports = cds.server
